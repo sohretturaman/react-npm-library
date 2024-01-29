d42c69d4dcd784db59d544cfd3c9bc1be86daffa
şöhrett <sohretturaman@gmail.com>
2024-01-29 22:20:19 +0300

    first npm react library has been published

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
-import { ExampleComponent } from 'strmn'
+import { DashedButton,DefalultButton,LinkButton,PrimaryButton,TextButton } from 'strmn'
 import 'strmn/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className='container'> 
+  {/*   <ExampleComponent text="Hello World" /> */}
+    <br/>
+    <PrimaryButton text="Primary Button" />
+    <DefalultButton text="Default Button" />
+    <DashedButton text="Dashed Button" />
+
+    <br/>
+    <br/>
+    <LinkButton text="Link Button" />
+    <TextButton text="Text Button" />
+
+    </div>
+  )
+
 }
+
 
 export default App
 
--- src/index.js
+++ src/index.js
@@ -4,3 +4,44 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+//write your comps in this file and test them  in app.js file under the example folder
+export const PrimaryButton = ({ text }) => {
+  return (
+    <div>
+      <button className={styles.primary}>{text}</button>
+    </div>
+  )
+}
+
+export const DefalultButton = ({ text }) => {
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={styles.default}>{text}</button>
+    </div>
+  )
+}
+
+export const DashedButton = ({ text }) => {
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={styles.dashed}>{text}</button>
+    </div>
+  )
+}
+
+export const TextButton = ({ text }) => {
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={styles.text}>{text}</button>
+    </div>
+  )
+}
+
+export const LinkButton = ({ text }) => {
+  return (
+    <div className={styles.buttonWrapper}>
+      <button className={styles.link}> {text}</button>
+    </div>
+  )
+}
